92ca3fd40ed0850f45da332ee4cdfecd8c6cc82d
Anton Ivanov <anton9032@gmail.com>
2024-05-29 19:26:03 +0300

    фыв

--- Ülesanne.py
+++ Ülesanne.py
@@ -46,11 +46,11 @@ def filter_by_hobby(people_list: list, hobby: str) -> list:
     :param hobby: hobby to filter by.
     :return: filtered list of people.
     """
+    filtered_people = []
     for person in people_list:
         if hobby in person.hobbies:
-            return person
-
-
+            filtered_people.append(person)
+    return filtered_people
 
 
 def sort_by_most_hobbies(people_list: list) -> list:
@@ -63,14 +63,7 @@ def sort_by_most_hobbies(people_list: list) -> list:
     :param people_list: list of people to sort.
     :return: sorted list of people.
     """
-    def most_hobbies_key(person):
-        return (-len(person.hobbies), person.full_name)
-
-    return sorted(people_list, key=most_hobbies_key)
-
-
-def least_hobbies_key(person):
-    return (len(person.hobbies), person.full_name)
+    return sorted(people_list, key=lambda person: (-len(person.hobbies), person.full_name))
 
 
 def sort_by_least_hobbies(people_list: list) -> list:
@@ -83,11 +76,7 @@ def sort_by_least_hobbies(people_list: list) -> list:
     :param people_list: list of people to sort.
     :return: sorted list of people.
     """
-    return sorted(people_list, key=least_hobbies_key)
-
-
-def full_name_key(person):
-    return person.full_name
+    return sorted(people_list, key=lambda person: (len(person.hobbies), person.full_name))
 
 
 def sort_people_and_hobbies(people_list: list) -> list:
@@ -99,10 +88,9 @@ def sort_people_and_hobbies(people_list: list) -> list:
     :param people_list: list of people to sort.
     :return: sorted list of people.
     """
-    sorted_people = sorted(people_list, key=full_name_key)
-    for person in sorted_people:
+    for person in people_list:
         person.hobbies.sort()
-    return sorted_people
+    return sorted(people_list, key=lambda person: person.full_name)
 
 
 if __name__ == '__main__':
@@ -110,9 +98,9 @@ if __name__ == '__main__':
     person2 = Person("Jeff", "Bezos", ["money", "hair", "late_capitalism", "space", "unions"])
     person3 = Person("Elon", "Musk", ["late_capitalism", "space", "cars"])
     people = [person1, person2, person3]
-    
+
     print(sort_by_most_hobbies(people))  # -> [JeffBezos, ElonMusk, MariKukk]
-    print(sort_by_least_hobbies(people))   # -> [ElonMusk, MariKukk, JeffBezos]
+    print(sort_by_least_hobbies(people))  # -> [ElonMusk, MariKukk, JeffBezos]
     print(filter_by_hobby(people, "space"))  # -> [ElonMusk, JeffBezos]
     print(sort_people_and_hobbies(people))  # -> [ElonMusk, JeffBezos, MariKukk]
     print(person1.hobbies)  # -> ['biking', 'dancing', 'programming']
